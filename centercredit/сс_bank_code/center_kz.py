784d141ec7215c6f5e3e806b4f7647ce633685b0
yusennn <116973490+yusennn@users.noreply.github.com>
2023-11-27 22:06:10 +0600

    new file:   centercredit/cc_bank_jsons/eng.json 	new file:   centercredit/cc_bank_jsons/kz.json 	new file:   centercredit/cc_bank_jsons/ru.json 	renamed:    centercredit/center_eng.pdf -> centercredit/cc_bank_pdfs/center_eng.pdf 	renamed:    centercredit/center_kz.pdf -> centercredit/cc_bank_pdfs/center_kz.pdf 	renamed:    centercredit/center_ru.pdf -> centercredit/cc_bank_pdfs/center_ru.pdf 	deleted:    centercredit/center_eng.py 	deleted:    centercredit/center_kz.py 	deleted:    centercredit/center_ru.py 	deleted:    centercredit/tempCodeRunnerFile.py 	new file:   "centercredit/\321\201\321\201_bank_code/center_eng.py" 	new file:   "centercredit/\321\201\321\201_bank_code/center_kz.py" 	new file:   "centercredit/\321\201\321\201_bank_code/center_ru.py" 	deleted:    centercredit_data.json 	deleted:    operation.py

---
+++ centercredit/сс_bank_code/center_kz.py
@@ -0,0 +1,93 @@
+import PyPDF2
+import re
+import json
+
+pdf_file_path = "cc_bank_pdfs/center_kz.pdf"
+def extract_text_from_pdf(pdf_file_path):
+    text = ""
+    with open(pdf_file_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text 
+
+if pdf_file_path != 0:
+    pdf_text = extract_text_from_pdf(pdf_file_path)
+else:
+    print("file is empty")
+
+# Поиск ФИО
+fio_re = r'\d{2}\.\d{2}\.\d{4}\s-\s\d{2}\.\d{2}.\d{4}\n(\D+?)\sЖСН'
+fio = re.search(fio_re, pdf_text).group(1)
+fio = ' '.join(fio.split())
+print(fio)
+
+# ИИН
+iin_re = r'ЖСН:\s(\d+)'
+iin = re.search(iin_re, pdf_text).group(0)
+iin = ' '.join(iin.split())
+print(iin)
+
+# Инфа об счете 
+# Валюта
+curr_re = r'Валюта\s+(\w+)'
+curr = re.search(curr_re, pdf_text).group(0)
+curr = ' '.join(curr.split())
+print(curr)
+# Карта
+card_re = r'Карта\s+\d+\*{6}\d+'
+card = re.search(card_re, pdf_text).group(0)
+card = ' '.join(card.split())
+print(card)
+# Счет
+acc_re = r'Түрі\s+(\w+)\s+(\w+)'
+acc = re.search(acc_re, pdf_text).group(0)
+acc = ' '.join(acc.split())
+print(acc)
+# Период
+per_re = r'Кезеңі\s+\d{2}\.\d{2}\.\d{4}\s-\s\d{2}\.\d{2}.\d{4}'
+per = re.search(per_re, pdf_text).group(0)
+per = ' '.join(per.split())
+print(per)
+
+header_data = {
+    "fio": fio,
+    "iin": iin,
+    "currency": curr,
+    "card": card,
+    "card_number": acc,
+    "period": per
+}
+
+data = []
+
+pdf_text = pdf_text.replace(chr(9), ' ')  # заменяем табы на пробелы
+pdf_text = pdf_text.replace('\n', ' ')  # заменяем табы на пробелы
+
+pos_start = pdf_text.find("Cashback, KZT")
+pos_start += len("Cashback, KZT")
+pdf_text = pdf_text[pos_start:]
+
+# print("->", pdf_text, "<-")
+
+row_re = r'(\d\d\.\d\d\.20\d\d +\d\d:\d\d:\d\d) +(\d\d\.\d\d\.20\d\d)(.*?)Плательщик:(.*?)(\-?[\d ]+,\d\d)'
+
+rows = re.findall(row_re, pdf_text)
+for row in rows:
+    result = {
+        "date_operation": row[0],
+        "date_show_account": row[1],
+        "details": row[2],
+        "payer": row[3],
+        "amount": row[4],
+    }
+    data.append(result)
+    print(result)
+
+centercredit_data = {
+    "header_data": header_data,
+    "transactions": data
+}
+with open('cc_bank_jsons/kz.json', 'w', encoding='utf-8') as json_file:
+    json.dump(centercredit_data, json_file, ensure_ascii=False, indent=4)
